aab87355de86d1865017a25d1bbb2b41fbf6f15a
harrj396 <ahletmethink@gmail.com>
2022-10-13 20:10:27 -0400

    Added JS to try and enable saving through local storage

--- assets/logic.js
+++ assets/logic.js
@@ -1,7 +1,14 @@
 // dom elements
 var container = document.querySelector('.container');
 
-// var for time during work day
+$("saveBtn").on("click"), function () {
+
+}
+
+// var for text blocks
+var nineJsBlock = $('9amBlock')
+
+// var for text area of blocks
 var nineAMJS = $('#nineAM');
 var tenAMJS = $('#tenAM');
 var elevenAMJS = $('#elevenAM');
@@ -12,10 +19,29 @@ var threePMJS = $('#threePM');
 var fourPMJS = $('#fourPM');
 var fivePMJS = $('#fivePM')
 
+
+
 // shows current time on screen
 var date = moment();
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
 
-container.innerHTML += `
-<div class="row>
-    <div class="col-`
+
+// function to save text
+function saveEvent (nineJsBlock) {
+    if (nineAMJS.value !== "") {
+        localStorage.setItem(nineAMJS.id, nineAMJS.value);
+    
+    } else if (nineAMJS.value === "" && localStorage.getItem(nineAMJS.id) !==null) {
+        localStorage.removeItem(nineJSBlock.id);
+    }
+}
+
+function buttonTest(event) {
+if (event.target.tagname === 'saveBtn') {
+    var textBox = event.target.parentNode.querySelector(".description");
+    saveEvent(textBox) }
+else if (event.target.target === 'I') {
+    var textBox = event.target.parentNode.parentNode.querySelector(".description");
+    saveEvent(textBox);
+    }
+}
